e173f9d484a265570130a2b0c4e02fcd5587ebbc
Gabriel Salles <136916555+1fja@users.noreply.github.com>
2026-01-31 17:39:25 -0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -122,3 +122,86 @@ window.addEventListener("beforeunload", function () {
   peerPublicKey = null
   socket = null
 })
+
+function generateEphemeralKey() {
+  return openpgp.generateKey({
+    type: "rsa",
+    rsaBits: 1024,
+    userIDs: [{ name: "ephemeral" }]
+  })
+}
+
+const _originalSendMsg = sendMsg
+
+sendMsg = function () {
+  if (!peerPublicKey || !socket || socket.readyState !== 1) {
+    alert("Waiting peer")
+    return
+  }
+
+  const input = document.getElementById("msg")
+  const text = input.value
+  if (!text) return
+  input.value = ""
+
+  generateEphemeralKey()
+    .then(keys => {
+      return openpgp.readKey({ armoredKey: keys.publicKey })
+        .then(ephPub => {
+          const pad = " ".repeat(Math.floor(Math.random() * 200) + 50)
+          const payload = text + "::PAD::" + pad
+
+          return openpgp.createMessage({ text: payload })
+            .then(msg => {
+              return openpgp.encrypt({
+                message: msg,
+                encryptionKeys: [peerPublicKey, ephPub]
+              })
+            })
+        })
+    })
+    .then(enc => {
+      const jitter = Math.floor(Math.random() * 400) + 100
+      setTimeout(() => {
+        socket.send(JSON.stringify({
+          type: "msg",
+          data: enc
+        }))
+      }, jitter)
+
+      addMsg("You: " + text)
+    })
+}
+
+function sendFakePGP() {
+  if (!peerPublicKey || !socket || socket.readyState !== 1) return
+
+  generateEphemeralKey()
+    .then(keys => {
+      return openpgp.readKey({ armoredKey: keys.publicKey })
+        .then(ephPub => {
+          return openpgp.createMessage({
+            text: Math.random().toString(36).repeat(8)
+          }).then(msg => {
+            return openpgp.encrypt({
+              message: msg,
+              encryptionKeys: [peerPublicKey, ephPub]
+            })
+          })
+        })
+    })
+    .then(enc => {
+      const jitter = Math.floor(Math.random() * 800) + 200
+      setTimeout(() => {
+        socket.send(JSON.stringify({
+          type: "msg",
+          data: enc
+        }))
+      }, jitter)
+    })
+}
+
+/* Loop de tráfego falso */
+setInterval(function () {
+  sendFakePGP()
+}, Math.random() * 4000 + 3000)
